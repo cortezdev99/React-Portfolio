9f7d72d2b989c8cc35fb7adeb1421b25b63af043
Michale-Cortez <CortezDevelopement@gmail.com>
2020-04-25 17:25:19 -0600

    added typewriter effect to text on mainpage

--- src/Components/layout/Homepage.js
+++ src/Components/layout/Homepage.js
@@ -7,12 +7,40 @@ class Homepage extends Component {
     return (
       <div className='homepage'>
         <div className="homepage__image">
-          <img src={image} alt="mountains"/>
+
+          {/* <img src={image} alt="mountains"/> */}
+          <div className='image'style={{backgroundImage: "url(" + image + ")"}}>
+            <div className="homepage__text typewriter-animation">
+              Hello there, my name's Michael.
+            </div>
+
+            <div className="homepage__text__2 typewriter-animation__2">
+              I'm a FullStack Software Developer.
+            </div>
+          </div>
+
+
         </div>
+
 
         <div className="homepage__heading">
           <div>Check out my projects!</div>
           <div className='line'></div>
+        </div>
+
+        <div className="homepage__project-filter">
+          <div className='homepage__project-filter__all btn'>
+            All
+          </div>
+          <div className='homepage__project-filter__react btn'>
+            React
+          </div>
+          <div className='homepage__project-filter__firebase btn'>
+            Firebase
+          </div>
+          <div className='homepage__project-filter__python btn'>
+            Python
+          </div>
         </div>
       </div>
     )
