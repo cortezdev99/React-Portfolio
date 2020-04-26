1a2a53573ff848170be0bf50587b253f4bfd767f
Michale-Cortez <CortezDevelopement@gmail.com>
2020-04-26 13:57:30 -0600

    can now send data to my database by submitted data from my form which then dispatches an action which then goes to my projectActions and dispatches either 'create project' or 'create project error'

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -8,6 +8,8 @@ const initState = {
 
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error', action.err)
     case 'CREATE_PROJECT':
       console.log('created project', action.project)
     default:
