15684e85b91ade3bdeeb9e83f99cc397aad898f4
Michale-Cortez <CortezDevelopement@gmail.com>
2020-04-26 12:01:27 -0600

    set up basic skeleton for creating a project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/layout/Navbar';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Homepage from './Components/layout/Homepage';
+import CreateProject from './Components/projects/CreateProject'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path='/' component={Homepage} />
+          <Route path='/create' component={CreateProject} />
         </Switch>
       </div>
     </Router>
--- src/Components/projects/CreateProject.js
+++ src/Components/projects/CreateProject.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function CreateProject() {
-  return (
-    <div>
-      
-    </div>
-  )
+class CreateProject extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      title: '',
+      content: '',
+      link: '',
+      type: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state)
+  }
+
+  render() {
+    return (
+      <div className='create-project__container'>
+        <form onSubmit={this.handleSubmit}>
+          <div className='create-project__input'>
+            <input type='text' id='title' placeholder='Title' onChange={this.handleChange} />
+          </div>
+
+          <div className='create-project__input'>
+            <input type='text' id='content' placeholder='Content' onChange={this.handleChange} />
+          </div>
+
+          <div className='create-project__input'>
+            <input type='text' id='link' placeholder='Link' onChange={this.handleChange} />
+          </div>
+
+          <div className='create-project__input'>
+            <input type='text' id='type' placeholder='Type' onChange={this.handleChange} />
+          </div>
+
+          <div className='create-project__button'>
+            <button>Create Project</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
+
+export default CreateProject
