3c84b030b2cb0b45781e1a5fd8df8673fd91268c
Michale-Cortez <CortezDevelopement@gmail.com>
2020-04-26 14:36:31 -0600

    setup project details component and also rendering individual project data in accordance to it

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/layout/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Homepage from './Components/layout/Homepage';
 import CreateProject from './Components/projects/CreateProject'
+import ProjectDetails from './Components/projects/ProjectDetails';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/create' component={CreateProject} />
+          <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
       </div>
     </Router>
--- src/Components/projects/ProjectDetails.js
+++ src/Components/projects/ProjectDetails.js
@@ -1,9 +1,52 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
-export default function ProjectDetails() {
-  return (
-    <div>
-      
-    </div>
-  )
+const ProjectDetails = (props) => {
+  const { project } = props;
+
+  if (project) {
+    return (
+      <div className='project-details__container'>
+        <div className='project-details__title'>
+          {project.title}
+        </div>
+  
+        <div className='project-details__content'>
+          {project.content}
+        </div>
+  
+        <div className='project-details__link'>
+          {project.link}
+        </div>
+  
+        <div className='project-details__type'>
+          {project.type}
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className='project-details__loading'>
+        Loading...
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id
+  const projects = state.firestore.data.projects
+  const project = projects ? projects[id] : null
+  return {
+    project: project
+  }
+}
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'projects' }
+  ])
+)(ProjectDetails)
