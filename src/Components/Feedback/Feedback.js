050d28eb834107db339ac330a7572f775b1b617e
Michale-Cortez <CortezDevelopement@gmail.com>
2020-05-02 16:56:26 -0600

    finished dark mode styles for non admin pages, now going to do the the same for admin pages

--- src/Components/Feedback/Feedback.js
+++ src/Components/Feedback/Feedback.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 
 import { createFeedback } from '../../store/actions/feedbackActions'
 import bannerImage from '../../images/feedbackImages/bannerImage4.jpg'
+import darkImage from '../../images/feedbackImages/darkBanner2.jpg'
 
 class Feedback extends Component {
   constructor() {
@@ -26,51 +27,93 @@ class Feedback extends Component {
   }
 
   render() {
-    const { sent, err } = this.props
-    return (
-      <div className="feedback-container">
+    const { sent, err, darkmode } = this.props
 
-        <div className='feedback-container__banner-image' style={{backgroundImage: "url(" + bannerImage + ")"}} />
-
-        <div className='feedback-container__header'>
-          <div className='line'>What do you like/dislike about my portfolio? <br /> Leave feedback!</div>
+    if (!darkmode) {
+      return (
+        <div className="feedback-container">
+  
+          <div className='feedback-container__banner-image' style={{backgroundImage: "url(" + bannerImage + ")"}} />
+  
+          <div className='feedback-container__header'>
+            <div className='line'>What do you like/dislike about my portfolio? <br /> Leave feedback!</div>
+          </div>
+  
+          { sent ? (
+            <div className='feedback-container__submitted'><div className='thanks'>Thank You!</div></div>
+          ) : (
+            <form onSubmit={this.handleSubmit}>
+              <div className='feedback-container__inputs'>
+                <input
+                  type="text"
+                  id="title"
+                  placeholder="Title"
+                  onChange={this.handleChange}
+                />
+  
+                <input
+                  type="text"
+                  id="reason"
+                  placeholder="Reason"
+                  onChange={this.handleChange}
+                />
+              </div>
+  
+              <div className="feedback-container__button">
+                <button>Submit</button>
+                { err ? <div className='error'>{err}</div> : null}
+              </div>
+            </form>
+          )}
         </div>
-
-        { sent ? (
-          <div className='feedback-container__submitted'><div className='thanks'>Thank You!</div></div>
-        ) : (
-          <form onSubmit={this.handleSubmit}>
-            <div className='feedback-container__inputs'>
-              <input
-                type="text"
-                id="title"
-                placeholder="Title"
-                onChange={this.handleChange}
-              />
-
-              <input
-                type="text"
-                id="reason"
-                placeholder="Reason"
-                onChange={this.handleChange}
-              />
-            </div>
-
-            <div className="feedback-container__button">
-              <button>Submit</button>
-              { err ? <div className='error'>{err}</div> : null}
-            </div>
-          </form>
-        )}
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div className="feedback-container dark-feedback-container">
+  
+          <div className='feedback-container__banner-image' style={{backgroundImage: "url(" + darkImage + ")"}} />
+  
+          <div className='feedback-container__header dark-feedback-container__header'>
+            <div className='line'>What do you like/dislike about my portfolio? <br /> Leave feedback!</div>
+          </div>
+  
+          { sent ? (
+            <div className='feedback-container__submitted dark-feedback-container__submitted'><div className='thanks'>Thank You!</div></div>
+          ) : (
+            <form onSubmit={this.handleSubmit}>
+              <div className='feedback-container__inputs dark-feedback-container__inputs'>
+                <input
+                  type="text"
+                  id="title"
+                  placeholder="Title"
+                  onChange={this.handleChange}
+                />
+  
+                <input
+                  type="text"
+                  id="reason"
+                  placeholder="Reason"
+                  onChange={this.handleChange}
+                />
+              </div>
+  
+              <div className="feedback-container__button dark-feedback-container__button">
+                <button>Submit</button>
+                { err ? <div className='error'>{err}</div> : null}
+              </div>
+            </form>
+          )}
+        </div>
+      )
+    }
   }
 }
 
 const mapStateToProps = (state) => {
   return {
     sent: state.feedback.sent,
-    err: state.feedback.err
+    err: state.feedback.err,
+    darkmode: state.darkmode.darkmode
   }
 }
 
--- src/Components/layout/Contact.js
+++ src/Components/layout/Contact.js
@@ -1,42 +1,90 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import bannerImage from '../../images/contactpageImages/contactpageBanner2.jpg'
+import darkBanner from '../../images/contactpageImages/darkBanner.jpg'
 
-const Contact = () => {
-  return (
-    <div className='contact-container'>
+class Contact extends Component {
+  render() {
+    const { darkmode } = this.props
 
-      <div className='contact-container__banner-image' style={{backgroundImage: "url(" + bannerImage + ")"}} />
-
-      <div className='contact-container__header'>
-        <div className='line'>Contact</div>
-      </div>
-
-      <div className='contact-container__main-content'>
-        <div className='contact-container__bullet-points contact-container__email'>
-          <div className='contact-container__shortan-width'>
-            <div className='icon'><FontAwesomeIcon icon="envelope" /></div>
-            <div className='contact-container__bullet-point'>CortezDevelopement@gmail.com</div>
+    if (!darkmode) {
+      return (
+        <div className='contact-container'>
+    
+          <div className='contact-container__banner-image' style={{backgroundImage: "url(" + bannerImage + ")"}} />
+    
+          <div className='contact-container__header'>
+            <div className='line'>Contact</div>
+          </div>
+    
+          <div className='contact-container__main-content'>
+            <div className='contact-container__bullet-points contact-container__email'>
+              <div className='contact-container__shortan-width'>
+                <div className='icon'><FontAwesomeIcon icon="envelope" /></div>
+                <div className='contact-container__bullet-point'>CortezDevelopement@gmail.com</div>
+              </div>
+            </div>
+    
+            <div className='contact-container__bullet-points contact-container__phone'>
+              <div className='contact-container__shortan-width'>
+                <div className='icon'><FontAwesomeIcon icon="phone" /></div>
+                <div className='contact-container__bullet-point'>801-259-4056</div>
+              </div>
+            </div>
+    
+            <div className='contact-container__bullet-points contact-container__location'>
+              <div className='contact-container__shortan-width'>
+                <div className='icon'><FontAwesomeIcon icon="map-marked-alt" /></div>
+                <div className='contact-container__bullet-point'>West Jordan, UT</div>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className='contact-container__bullet-points contact-container__phone'>
-          <div className='contact-container__shortan-width'>
-            <div className='icon'><FontAwesomeIcon icon="phone" /></div>
-            <div className='contact-container__bullet-point'>801-259-4056</div>
+      )
+    } else {
+      return (
+        <div className='contact-container dark-contact-container'>
+    
+          <div className='contact-container__banner-image' style={{backgroundImage: "url(" + darkBanner + ")"}} />
+    
+          <div className='contact-container__header dark-contact-container__header'>
+            <div className='line'>Contact</div>
+          </div>
+    
+          <div className='contact-container__main-content'>
+            <div className='contact-container__bullet-points contact-container__email dark-contact-container__bullet-points'>
+              <div className='contact-container__shortan-width'>
+                <div className='icon'><FontAwesomeIcon icon="envelope" /></div>
+                <div className='contact-container__bullet-point'>CortezDevelopement@gmail.com</div>
+              </div>
+            </div>
+    
+            <div className='contact-container__bullet-points contact-container__phone dark-contact-container__bullet-points'>
+              <div className='contact-container__shortan-width'>
+                <div className='icon'><FontAwesomeIcon icon="phone" /></div>
+                <div className='contact-container__bullet-point'>801-259-4056</div>
+              </div>
+            </div>
+    
+            <div className='contact-container__bullet-points contact-container__location dark-contact-container__bullet-points'>
+              <div className='contact-container__shortan-width'>
+                <div className='icon'><FontAwesomeIcon icon="map-marked-alt" /></div>
+                <div className='contact-container__bullet-point'>West Jordan, UT</div>
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className='contact-container__bullet-points contact-container__location'>
-          <div className='contact-container__shortan-width'>
-            <div className='icon'><FontAwesomeIcon icon="map-marked-alt" /></div>
-            <div className='contact-container__bullet-point'>West Jordan, UT</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+      )
+    }
+  }
 }
 
-export default Contact
+const mapStateToProps = (state) => {
+  return {
+    darkmode: state.darkmode.darkmode
+  }
+}
+
+export default connect(mapStateToProps)(Contact)
